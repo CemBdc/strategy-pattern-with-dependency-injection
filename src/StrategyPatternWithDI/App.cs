380350bc5cea89512a186a756855185876d48948
cembideci <cembdci@gmail.com>
2020-03-15 14:55:06 +0300

    Dependency Injection concept is implemented.

---
+++ src/StrategyPatternWithDI/App.cs
@@ -0,0 +1,42 @@
+﻿using StrategyPatternWithDI.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPatternWithDI
+{
+    public class App
+    {
+        private readonly ICampaignStrategy _campaignStrategy;
+
+        public App(ICampaignStrategy campaignStrategy)
+        {
+            _campaignStrategy = campaignStrategy;
+        }
+
+        public void Run()
+        {
+            var campaign = _campaignStrategy.Resolve(CampaignTypes.Amount);
+            var isValidCampaign = campaign.IsValidCampaign(10, 8);
+            var discount = campaign.Discount(1500, 300);
+
+            Console.WriteLine($"CampaignType: {campaign.CampaignTypes}");
+            Console.WriteLine($"Total Amount: {1500}, Discount Amount: {300}");
+            Console.WriteLine($"Is Valid Campaign: {isValidCampaign}");
+            Console.WriteLine($"Discount: {discount}");
+
+            Console.WriteLine("-------------");
+
+            campaign = _campaignStrategy.Resolve(CampaignTypes.Rate);
+            isValidCampaign = campaign.IsValidCampaign(28, 12);
+            discount = campaign.Discount(1500, 50);
+
+            Console.WriteLine($"CampaignType: {campaign.CampaignTypes}");
+            Console.WriteLine($"Total Amount: {1500}, Discount Amount: {50}");
+            Console.WriteLine($"Is Valid Campaign: {isValidCampaign}");
+            Console.WriteLine($"Discount: {discount}");
+
+            Console.ReadLine();
+        }
+    }
+}
--- src/StrategyPatternWithDI/Program.cs
+++ src/StrategyPatternWithDI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using StrategyPatternWithDI.Operations;
+using StrategyPatternWithDI.Strategy;
+using System;
 
 namespace StrategyPatternWithDI
 {
@@ -6,7 +9,26 @@ namespace StrategyPatternWithDI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = ConfigureServices();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // calls the Run method in App, which is replacing Main
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddScoped<ICampaignStrategy, CampaignStrategy>();
+            services.AddScoped<ICampaign, AmountCampaign>();
+            services.AddScoped<ICampaign, RateCampaign>();
+
+            // required to run the application
+            services.AddTransient<App>();
+
+            return services;
         }
     }
 }
