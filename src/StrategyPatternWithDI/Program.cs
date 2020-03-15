0ad25ab9cbfddfe755a153fe3cb62d3faa623ceb
cembideci <cembdci@gmail.com>
2020-03-15 15:07:33 +0300

    The classes that implement ICampaign added into ServiceCollection by dynamically.

--- src/StrategyPatternWithDI/Program.cs
+++ src/StrategyPatternWithDI/Program.cs
@@ -2,6 +2,7 @@
 using StrategyPatternWithDI.Operations;
 using StrategyPatternWithDI.Strategy;
 using System;
+using System.Linq;
 
 namespace StrategyPatternWithDI
 {
@@ -22,8 +23,15 @@ namespace StrategyPatternWithDI
             IServiceCollection services = new ServiceCollection();
 
             services.AddScoped<ICampaignStrategy, CampaignStrategy>();
-            services.AddScoped<ICampaign, AmountCampaign>();
-            services.AddScoped<ICampaign, RateCampaign>();
+
+            //services.AddScoped<ICampaign, AmountCampaign>();
+            //services.AddScoped<ICampaign, RateCampaign>();
+
+            var types = typeof(Program).Assembly.DefinedTypes.Where(x => x.GetInterfaces().Contains(typeof(ICampaign)));
+            foreach (var type in types)
+            {
+                services.Add(new ServiceDescriptor(typeof(ICampaign), type, ServiceLifetime.Transient));
+            }
 
             // required to run the application
             services.AddTransient<App>();
